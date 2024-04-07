bb031f2b5a3b71c2db1e03f277c618135a38dc22
Ekigonzalez <exequielgonzalezbasacco@gmail.com>
2024-04-07 21:23:48 +0200

    Agregado de 2 scripts

--- Estilo tras otro.jsx
+++ Estilo tras otro.jsx
@@ -1,4 +1,4 @@
-// Script to apply a selected paragraph style in consecutive ocurrences of another selected paragraph style
+// Script to apply a selected paragraph style after another ocurrence of another paragraph style
 var doc = app.activeDocument;
 var firstStyle;
 var secondStyle;
@@ -13,14 +13,12 @@ function init() {
     // Iterate through the paragraphs
     for (var i = 0; i < doc.stories.length; i++) {
       var paragraphs = doc.stories[i].paragraphs;
-      for (var j = paragraphs.length - 1; j >= 0; j--) {
+      for (var j = 0; j < paragraphs.length; j++) {
         var currentParagraph = paragraphs[j];
-        var prevParagraph = paragraphs[j - 1];
+        var nextParagraph = paragraphs[j + 1];
 
         if (currentParagraph.appliedParagraphStyle.name == firstStyle) {
-          if (prevParagraph.appliedParagraphStyle.name == firstStyle) {
-            currentParagraph.appliedParagraphStyle = doc.paragraphStyles.itemByName(secondStyle);
-          }
+          nextParagraph.appliedParagraphStyle = doc.paragraphStyles.itemByName(secondStyle);
         }
       }
     }
@@ -42,16 +40,16 @@ function getStyles(doc) {
 // Show a dialog to let the user select the two paragraph styles
 function showDialog(stylesArray) {
 
-  var dlg = new Window("dialog", "Replace consecutive styles");
+  var dlg = new Window("dialog", "Apply style after another");
   dlg.orientation = "column";
   dlg.alignChildren = "left";
-  dlg.add ("statictext", undefined, "Original style:");
+  dlg.add ("statictext", undefined, "First style:");
 
   // Fill both dropdown elements with the array of paragraph styles
   var dropdownFirstStyle = dlg.add("dropdownlist", undefined, stylesArray);
   dropdownFirstStyle.selection = 3;
 
-  dlg.add ("statictext", undefined, "Replace with:");
+  dlg.add ("statictext", undefined, "Apply style to next paragraph:");
   var dropdownSecondStyle = dlg.add("dropdownlist", undefined, stylesArray);
   dropdownSecondStyle.selection = 4;
 
